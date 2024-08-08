b5d53a15de578433208a01020d53d630266d1f7d
Ian Volkov <ian.volkov@kcl.ac.uk>
2024-08-09 00:11:33 +0100

    Headings for graph

--- app/src/main/java/com/example/bloodpressure/gatt/BloodPressureAdapter.java
+++ app/src/main/java/com/example/bloodpressure/gatt/BloodPressureAdapter.java
@@ -1,4 +1,5 @@
 package com.example.bloodpressure.gatt;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,7 +12,10 @@ import com.example.bloodpressure.R;
 
 import java.util.List;
 
-public class BloodPressureAdapter extends RecyclerView.Adapter<BloodPressureAdapter.ViewHolder> {
+public class BloodPressureAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private static final int TYPE_HEADER = 0;
+    private static final int TYPE_ITEM = 1;
 
     private final List<BloodPressureReading> readings;
 
@@ -19,32 +23,53 @@ public class BloodPressureAdapter extends RecyclerView.Adapter<BloodPressureAdap
         this.readings = readings;
     }
 
+    @Override
+    public int getItemViewType(int position) {
+        if (position == 0) {
+            return TYPE_HEADER;
+        }
+        return TYPE_ITEM;
+    }
+
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_blood_pressure, parent, false);
-        return new ViewHolder(view);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (viewType == TYPE_HEADER) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_blood_pressure_header, parent, false);
+            return new HeaderViewHolder(view);
+        } else {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_blood_pressure, parent, false);
+            return new ItemViewHolder(view);
+        }
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        BloodPressureReading reading = readings.get(position);
-        holder.systolicTextView.setText(String.valueOf(reading.getSystolic()));
-        holder.diastolicTextView.setText(String.valueOf(reading.getDiastolic()));
-        holder.pulseTextView.setText(String.valueOf(reading.getPulse()));
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof ItemViewHolder) {
+            BloodPressureReading reading = readings.get(position - 1); // -1 because position 0 is header
+            ((ItemViewHolder) holder).systolicTextView.setText(String.valueOf(reading.getSystolic()));
+            ((ItemViewHolder) holder).diastolicTextView.setText(String.valueOf(reading.getDiastolic()));
+            ((ItemViewHolder) holder).pulseTextView.setText(String.valueOf(reading.getPulse()));
+        }
     }
 
     @Override
     public int getItemCount() {
-        return readings.size();
+        return readings.size() + 1;
     }
 
-    static class ViewHolder extends RecyclerView.ViewHolder {
+    static class HeaderViewHolder extends RecyclerView.ViewHolder {
+        HeaderViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+    static class ItemViewHolder extends RecyclerView.ViewHolder {
         TextView systolicTextView;
         TextView diastolicTextView;
         TextView pulseTextView;
 
-        ViewHolder(View itemView) {
+        ItemViewHolder(View itemView) {
             super(itemView);
             systolicTextView = itemView.findViewById(R.id.systolic);
             diastolicTextView = itemView.findViewById(R.id.diastolic);
